ba907fe641572b4f354b5007296e37421ca5c665
Shady Hussein <shadyhussein99@gmail.com>
2023-06-17 13:55:14 +0300

    Added sign in component and Firebase utilities

--- src/routes/signIn/SignIn.jsx
+++ src/routes/signIn/SignIn.jsx
@@ -1,16 +1,19 @@
-import { auth, googleAuth } from "../../utilities/folder/Firebase";
-import { signInWithPopup } from "firebase/auth";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utilities/folder/Firebase";
+
 
 function SignIn() {
-  const signInWithGooglePopup = async () => {
-    const response = await signInWithPopup(auth, googleAuth);
-    console.log(response);
+  const logUserGoogle = async () => {
+    const { user } = await signInWithGooglePopup()    // user: is the user information returned when the user sign in
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
     <div>
       <h1>Sign in page</h1>
-      <button onClick={signInWithGooglePopup}>Sign In with Google</button>
+      <button onClick={logUserGoogle}>Sign In with Google</button>
     </div>
   );
 }
--- src/utilities/folder/Firebase.js
+++ src/utilities/folder/Firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore"; // doc: used to specify the document in which collection by the id, getDoc: used to retrieve the required document, setDoc: used to addDoc
 
 const firebaseConfig = {
   apiKey: "AIzaSyDcvap-Yg5Kb7AMyLUShqVrQ6g9q5lTPR0",
@@ -13,6 +13,7 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
+// Authentication
 export const auth = getAuth(firebaseApp);
 export const googleAuth = new GoogleAuthProvider();
 
@@ -20,5 +21,32 @@ googleAuth.setCustomParameters({
   prompt: "select_account", // To force users to select an account when interacting with GoogleAuthProvider
 });
 
-export const database = getFirestore(firebaseApp)
-// In udemy project he imported doc, getDoc, setDoc from firebase/firestore
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleAuth);  // Function used to sign in with Google Popup
+
+
+// Storing the Authenticated users in the database
+export const database = getFirestore(firebaseApp);
+
+export const createUserDocumentFromAuth = async (userAuth) => {  // userAuth: is the user information returned when the user sign in
+
+  const userDocRef = doc(database, "users", userAuth.uid); // users: is the name of the collection that we want to dsve the users documents in, userAuth.uid: the user's information returned when the user sign in has a unique id called (uid) we use it to identify the document
+  
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {   // Check if the user that signed in doesn't exist in the collection (the user signed in in with Google for the first time), if so create a document for the user in the collection with the name and email which we destructured from the returned user information and add the date that the user sign in for the first time
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef; // To return the user document if he signed in before or it is the first time and we created a document for him, return the document anyway
+};
