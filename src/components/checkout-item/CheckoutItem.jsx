05a84cad7eb118e14400fea6552bcc93bb511f1b
Shady Hussein <shadyhussein99@gmail.com>
2023-06-20 14:13:18 +0300

    created checkout page with all its functionality

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -1,24 +1,21 @@
-
+import "./checkoutItem.css";
 
 function CheckoutItem(props) {
-
   return (
-    <div>
-      <img src={props.image} alt="product-image" />
-
-      <span>{props.name}</span>
-
-      <span>
-      <span onClick={props.decreaseQuantity}>{`<`}</span>
-      <span>{props.quantity}</span>
-      <span onClick={props.increaseQuantity}>{`>`}</span>
+    <div className="checkout-item-container">
+      <div className="image-container">
+        <img src={props.image} alt={props.name} />
+      </div>
+      <span className="name">{props.name}</span>
+      <span className="quantity">
+        <div onClick={props.decreaseQuantity} className="arrow">{`<`}</div>
+        <span className="value">{props.quantity}</span>
+        <div onClick={props.increaseQuantity} className="arrow">{`>`}</div>
       </span>
-
-      <p>{props.price}</p>
-
-      <span onClick={props.removeItem}>x</span>
+      <span className="price">{props.price}</span>
+      <div onClick={props.removeItem} className="remove-button">x</div>
     </div>
-  )
+  );
 }
 
-export default CheckoutItem
+export default CheckoutItem;
--- src/routes/checkout/Checkout.jsx
+++ src/routes/checkout/Checkout.jsx
@@ -1,47 +1,71 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { CartContext } from "../../contexts/CartContext";
 
 import CheckoutItem from "../../components/checkout-item/CheckoutItem";
 
+import "./checkout.css";
+
 function Checkout() {
+  const [totalCartPrice, setTotalCartPrice] = useState(0);
+
   const { cartItems, setCartItems } = useContext(CartContext);
 
+  const removeItem = (product) => {
+    const newCartItems = cartItems.filter((cartItem) => {
+      return cartItem.id !== product.id;
+    });
+    setCartItems(newCartItems);
+  };
+
   const decreaseQuantity = (product) => {
-    const newCartItems = cartItems.map(cartItem => {
-        if (cartItem.quantity > 0) {
-        return (
-        cartItem.id === product.id ? {...cartItem, quantity: cartItem.quantity - 1} :
-        cartItem
-    )} else {
-        return cartItem;
-    }})
-     setCartItems(newCartItems)
-  }
+    if (product.quantity > 1) {
+      const newCartItems = cartItems.map((cartItem) => {
+        return cartItem.id === product.id
+          ? { ...cartItem, quantity: cartItem.quantity - 1 }
+          : cartItem;
+      });
+      setCartItems(newCartItems);
+    } else {
+      removeItem(product);
+    }
+  };
 
   const increaseQuantity = (product) => {
-    const newCartItems = cartItems.map(cartItem => { return (
-        cartItem.id === product.id ? {...cartItem, quantity: cartItem.quantity + 1} :
-        cartItem
-    )})
-     setCartItems(newCartItems)
-  }
+    const newCartItems = cartItems.map((cartItem) => {
+      return cartItem.id === product.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem;
+    });
+    setCartItems(newCartItems);
+  };
 
-  const removeItem = (product) => {
-    const newCartItems = cartItems.filter(cartItem => { return (
-      cartItem.id !== product.id
-     )})
-     setCartItems(newCartItems)
-  }
+  useEffect(() => {
+    const totalPrice = cartItems.reduce((accumulator, currentElement) => {
+      return accumulator + currentElement.quantity * currentElement.price;
+    }, 0);    // In .reduce, the second argument (0) is the initial state of the accumulator
+
+    setTotalCartPrice(totalPrice);
+  }, [cartItems]);
 
 
   return (
-    <div>
-      <div>
-        <span>Product</span>
-        <span>Description</span>
-        <span>Quantity</span>
-        <span>Price</span>
-        <span>Remove</span>
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
       </div>
 
       {cartItems.map((product) => {
@@ -58,6 +82,8 @@ function Checkout() {
           />
         );
       })}
+
+      <span className="total">Total: ${totalCartPrice}</span>
     </div>
   );
 }
