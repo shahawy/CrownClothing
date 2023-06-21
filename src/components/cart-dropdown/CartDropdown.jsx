9ff85554d5ad4dc7dac40af55cbdb3c662991c39
Shady Hussein <shadyhussein99@gmail.com>
2023-06-21 23:56:43 +0300

    Adding Redux to manage state

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -13,19 +13,25 @@ function CartDropdown() {
 
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems.map((product) => {
-          return (
-            <CartItem
-              key={product.id}
-              image={product.imageUrl}
-              name={product.name}
-              price={product.price}
-              quantity={product.quantity}
-            />
-          );
-        })}
-      </div>
+      {cartItems.length > 0 ? (
+        <>
+          <div className="cart-items">
+            {cartItems.map((product) => {
+              return (
+                <CartItem
+                  key={product.id}
+                  image={product.imageUrl}
+                  name={product.name}
+                  price={product.price}
+                  quantity={product.quantity}
+                />
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <p className="empty-message">Your Cart is empty</p>
+      )}
 
       <Link to="/checkout">
         <Button buttonName="Go To Checkout" />
