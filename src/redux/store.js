bfa077c02fd06f468cc0fbdb847af80f43ff0ca2
Shady Hussein <shadyhussein99@gmail.com>
2023-06-23 19:47:38 +0300

    Adding Redux to manage state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+export default configureStore({
+  reducer: {},
+})
+
+
+
+
+
+
+
+
+/*
 // This is how "redux-persist" library works in the store file of redux
 
 import {createStore} from "react-redux"
@@ -9,7 +23,10 @@ import storage from "redux-persist/lib/storage"  // This what accesses the local
 const persistConfig = {
     key: "",   // the name of the reducer or the slice we want to persist as a string
     storage: storage,
-    blacklist: [],  // this is array of strings of the reducers or the the slice names that we don't want to persist, in our case we should put in the array the slice of the (User) because onAuthChanged() method of firestore handles it in the same way, so if we used redux-persist also with (User) slice there may be conflicts and issues, if we will not use onAuthChangedMethod() to keep the user logged in on refreshing the page, then we will use (User) slice with "redux-persist" normally
+    blacklist: [""],  // this is array of strings of the reducers or the the slice names that we don't want to persist, in our case we should put in the array the slice of the (User) because onAuthChanged() method of firestore handles it in the same way, so if we used redux-persist also with (User) slice there may be conflicts and issues, if we will not use onAuthChangedMethod() to keep the user logged in on refreshing the page, then we will use (User) slice with "redux-persist" normally
+    whitelist: [""]   // this is array of strings of the reducers or the the slice names that we want to persist, in our case we may persist the Cart slice only as we want it to persist on refreshing the page to save the products that the user has chosen, because User slice is handled by onAuthChanged() method of firebase and Categories Slice we don't need to persist it
+    
+    // We can either use (blacklist or whitelist) property to indicate the reducers or slices that we want to persist
 }
 
 const persistedReducer = persistReducer(persistConfig, _)  // The second argument should be the reducer that contains the whole reducers or slices
@@ -17,3 +34,4 @@ const persistedReducer = persistReducer(persistConfig, _)  // The second argumen
 export const store = createStore(persistedReducer)
 
 export const persistor = persistStore(store)
+*/
