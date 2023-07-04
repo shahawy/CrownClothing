9fc53b3d991fa8bb32c852ebd1a13de2d97a51e4
Shady Hussein <shadyhussein99@gmail.com>
2023-07-04 19:24:23 +0300

    Testing ProductCard component

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,9 +1,40 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addPresentItemsToCart,
+  addNewItemsToCart,
+} from "../../redux/cartSlice";
+
+import { useNavigate } from "react-router-dom";
+
 import Button from "../button/Button";
 
 import "./productCard.css";
 
 
 function ProductCard(props) {
+
+  const navigate = useNavigate()
+
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart.value);
+  const currentUser = useSelector((state) => state.user.value);
+
+  const addToCartClick = (productData) => {
+    if (currentUser) {
+      const existingProduct = cartItems.find(
+        (product) => product?.id === productData.id
+      );
+  
+      if (existingProduct) {
+        dispatch(addPresentItemsToCart(productData));
+      } else {
+        dispatch(addNewItemsToCart(productData));
+      }
+    } else {
+      navigate("/authentication")
+    }
+    
+  };
 
   return (
     <div className="product-card-container">
@@ -13,7 +44,7 @@ function ProductCard(props) {
         <span className="price">${props.price}</span>
       </div>
 
-      <Button onClick={props.addToCartClick} buttonName="Add To Cart" buttonType="inverted" />
+      <Button onClick={addToCartClick} buttonName="Add To Cart" buttonType="inverted" />
     </div>
   );
 }
