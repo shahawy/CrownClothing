5647d2193049f5c94b518798cd7396153716a93a
Shady Hussein <shadyhussein99@gmail.com>
2023-07-04 17:27:15 +0300

    Testing NavigationBar component

--- src/routes/navigationBar/__tests__/NavigationBar.test.js
+++ src/routes/navigationBar/__tests__/NavigationBar.test.js
@@ -1,20 +1,32 @@
-import { renderWithProviders } from "../../../utilities/tests/test-utils";
+// Testing components that rely on redux
+
 import { screen } from "@testing-library/react";
+
+// We no longer use render of @testing-library/react, we use our custom render for redux
+import { renderWithProviders } from "../../../utilities/tests/test-utils";
+
 import NavigationBar from "../NavigationBar";
 
+
 describe("Test Navigation Bar", () => {
-  test("Render Sign In link if there is no users", () => {
-    renderWithProviders(<NavigationBar />, {
+  
+  test("Render Sign In link and not Sign Out if there is no users", () => {
+    renderWithProviders(<NavigationBar />, {   // we used our custom render with redux to be able to test a state using (preloadedState)
       preloadedState: {
-        user: {
-          value: null,
+        user: {    // the slice name
+          value: null,  // the state needed to be modified to test
         },
       },
     });
 
-    const signInlinkElement = screen.getByText("SIGN IN");
-    expect(signInlinkElement).toBeInTheDocument();
-  });
+    const signInLinkElement = screen.getByText("SIGN IN");
+    expect(signInLinkElement).toBeInTheDocument();
+
+    const signOutLinkElement = screen.queryByText("SIGN OUT");
+    expect(signOutLinkElement).toBeNull();
+  });    // The difference between (getByText) and (queryByText) is that, when (queryByText) finds nothing it gives null,
+         // but when (getByText) finds nothing it throws an error
+
 
   test("Should not render Sign Out link and not Sign In if there is current user", () => {
     renderWithProviders(<NavigationBar />, {
@@ -25,10 +37,64 @@ describe("Test Navigation Bar", () => {
       },
     });
 
-    const signOutlinkElement = screen.getByText("SIGN OUT");
-    expect(signOutlinkElement).toBeInTheDocument();
+    const signOutLinkElement = screen.getByText("SIGN OUT");
+    expect(signOutLinkElement).toBeInTheDocument();
 
-    const signInlinkElement = screen.queryByText("SIGN IN");
-    expect(signInlinkElement).toBeNull();
-  });    // The difference between (getByText) and (queryByText) is that, when (queryByText) finds nothing it gives null,
-});      // but when (getByText) finds nothing it throws an error
+    const signInLinkElement = screen.queryByText("SIGN IN");
+    expect(signInLinkElement).toBeNull();
+  });
+
+
+  test("Should not render CartDropdown when isCartOpen is false", () => {
+    renderWithProviders(<NavigationBar />, {
+      preloadedState: {
+        cart: {
+          isCartOpen: false
+        },
+      },
+    });
+
+    const buttonElement = screen.queryByText("Go To Checkout");
+    expect(buttonElement).toBeNull();
+  });
+
+
+  test("Should render CartDropdown when isCartOpen is true and not show empty cart when there is items", () => {
+    const initialCartItems = [{id: 1, name: "Item A", imageUrl: "test", price: 10, quantity: 1}]
+
+    renderWithProviders(<NavigationBar />, {
+      preloadedState: {
+        cart: {
+          isCartOpen: true,
+          value: initialCartItems
+        },
+      },
+    });
+
+    const buttonElement = screen.getByRole("button");
+    expect(buttonElement).toBeInTheDocument();    // To be sure that CartDropdown component is rendered
+
+    const emptyCartElement = screen.queryByText("Your Cart is empty");
+    expect(emptyCartElement).toBeNull();    // To be sure that Cart is not empty when there is items in it
+  });
+
+
+  test("Should render CartDropdown when isCartOpen is true and show empty cart when there is no items", () => {
+    renderWithProviders(<NavigationBar />, {
+      preloadedState: {
+        cart: {
+          isCartOpen: true,
+          value: []
+        },
+      },
+    });
+
+    const buttonElement = screen.getByText("Go To Checkout");
+    expect(buttonElement).toBeInTheDocument();    // To be sure that CartDropdown component is rendered
+
+    const emptyCartElement = screen.getByText("Your Cart is empty");
+    expect(emptyCartElement).toBeInTheDocument();    // To be sure that Cart is empty when there is no items in it
+  });
+});
+
+  
