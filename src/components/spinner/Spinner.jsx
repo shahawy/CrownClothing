9c617a65dabde449849d90be6466e0a5fd083dd4
Shady Hussein <shadyhussein99@gmail.com>
2023-07-06 17:17:26 +0300

    Testing CategoriesPreview component

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -2,7 +2,7 @@ import "./spinner.css";
 
 function Spinner() {
   return (
-    <div className="spinner-overlay" data-testid="spinner">
+    <div className="spinner-overlay" data-testid="spinner">  {/* data-testid: is for testing */}
       <div className="spinner-container"></div>
     </div>
   );
