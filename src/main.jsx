09268f50f78c3d5460d67b547f344db42d244d18
shadyhussein <shadyhussein99@gmail.com>
2023-11-03 01:41:54 +0200

    Added Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 
+import ReactGA from "react-ga4";  // For Google Analytics
+
 // import { UserProvider } from "./contexts/UserContext";
 // import { CategoriesProvider } from "./contexts/CategoriesContext";
 // import { CartProvider } from "./contexts/CartContext.jsx";
@@ -17,6 +19,13 @@ import { Elements } from "@stripe/react-stripe-js" // The component that gives t
 import { stripePromise } from "./utilities/stripe/stripe.js";
 
 import "./main.css";
+
+
+ReactGA.initialize("your GA measurement id");
+
+// Need to know how to add more hit types like "scroll", which Captures scroll events each time a visitor gets to the bottom of a page 
+ReactGA.send({ hitType: "pageview", page: window.location.pathname});
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
